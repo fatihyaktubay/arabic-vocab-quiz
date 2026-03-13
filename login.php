78539b2f57344f208880a1905310e2fac41e145b
fatihyaktubay <265016689+fatihyaktubay@users.noreply.github.com>
2026-03-13 13:04:34 +0300

    code for admin login

--- login.php
+++ login.php
@@ -1,1 +1,29 @@
+<?php
 
+session_start();
+header('Content-Type: application/json');
+
+$ADMIN_USER = "admin";
+$ADMIN_PASS = "myStrongPassword123";
+
+$input = json_decode(file_get_contents('php://input'), true);
+
+$user = $input['username'] ?? '';
+$pass = $input['password'] ?? '';
+
+if ($user === $ADMIN_USER && $pass === $ADMIN_PASS) {
+
+    $_SESSION['admin'] = true;
+
+    echo json_encode([
+        "success" => true
+    ]);
+
+} else {
+
+    http_response_code(401);
+
+    echo json_encode([
+        "success" => false
+    ]);
+}
